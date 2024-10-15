4c5247757dde394c2bb6b85fc6cd3022803ab56c
csy2617 <suyen.chia05@gmail.com>
2024-10-15 15:58:29 +1100

    End of Section 8

---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+    id: number;
+    name: string;
+    description: string;
+    price:number;
+    pricturUrl: string;
+    type: string;
+    brand: string;
+    quantityInStock: number;
+}
