90ac2fd7bf4b3974d36cf4ed25adeb1aa5124e6b
csy2617 <suyen.chia05@gmail.com>
2024-10-22 17:58:56 +1100

    End of Section 10

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {MatBadge} from '@angular/material/badge';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,10 @@ import {MatBadge} from '@angular/material/badge';
   imports: [
     MatIcon,
     MatButton,
-    MatBadge
+    MatBadge,
+    RouterLink,
+    RouterLinkActive,
+    RouterModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
